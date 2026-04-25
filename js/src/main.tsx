79f5534e5755199f559705e96ae88f879f547dd9
Arshadul Monir <arshadulmonir@gmail.com>
2026-04-24 21:13:38 -0400

    Created Initial Frontend

---
+++ js/src/main.tsx
@@ -0,0 +1,32 @@
+// THIS MUST BE FIRST. NEVER MOVE THIS.
+import "@/patches";
+import "@mantine/core/styles.css";
+import ReactQueryProvider from "@/lib/queryProvider";
+import { router } from "@/lib/router";
+import "@mantine/notifications/styles.css";
+import { themeOverride } from "@/lib/theme";
+import { MantineProvider } from "@mantine/core";
+import "@mantine/dates/styles.css";
+import { Notifications } from "@mantine/notifications";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router";
+
+// if (import.meta.env.VITE_MOCK === "true") {
+//   const { launchMockServer } = await import("@/__mock__");
+//   await launchMockServer();
+// }
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ReactQueryProvider>
+      <MantineProvider theme={themeOverride} forceColorScheme={"dark"}>
+        <RouterProvider router={router} />
+        <Notifications />
+      </MantineProvider>
+      <ReactQueryDevtools />
+    </ReactQueryProvider>
+  </StrictMode>,
+);
