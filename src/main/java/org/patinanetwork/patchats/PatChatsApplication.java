91c6f613422dfaef0f700bccc87f257dab74017f
Arshadul Monir <arshadulmonir@gmail.com>
2026-04-24 22:08:13 -0400

    Setup intial backend

---
+++ src/main/java/org/patinanetwork/patchats/PatChatsApplication.java
@@ -0,0 +1,19 @@
+package org.patinanetwork.patchats;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+@Slf4j
+public class PatChatsApplication {
+
+    public static void main(final String[] args) {
+        Thread.setDefaultUncaughtExceptionHandler(
+                (thread, throwable) -> log.error("Uncaught exception in thread: {}", thread.getName(), throwable));
+
+        SpringApplication.run(PatChatsApplication.class, args);
+    }
+}
