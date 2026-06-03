fefb7431d2b450e0c50b9f9fc2273fde06f3d6b3
Isabella Lam <isabellalam12@gmail.com>
2026-06-03 09:09:29 -0400

    52: Updated parse implementation to use TS and PapaParse api

---
+++ js/src/app/user/admin/emails/parseCSV.ts
@@ -0,0 +1,96 @@
+import fs from 'fs';
+import Papa from 'papaparse';
+
+const USER_FILE = './users-test.csv';
+const PAIR_FILE = './pairings-test.csv';
+
+function parseUserFile(filePath: string): Map<string, User> {
+  const userFile = fs.readFileSync(new URL(filePath, import.meta.url), 'utf8');
+
+  const config = {
+    quotes: false,
+    quoteChar: '"',
+    escapeChar: '"',
+    delimiter: ",",
+    header: true,
+    newline: "\n",
+    skipEmptyLines: true,
+    columns: null
+  };
+
+  interface User { 
+    name : string; 
+    email: string;
+    intro : string;
+    linkedin : string;
+    industry : string;
+    preferences : string;
+    topics : string;
+    anything : string;
+  }
+
+  const results = Papa.parse(userFile, config);
+  const userMap = new Map<string, User>();
+  
+  for (const userData of results.data) {
+    const user: User = {
+      name: userData.Name,
+      email: userData.Email,
+      intro: userData.Intro, 
+      linkedin: userData.LinkedIn,
+      industry: userData.Industry,
+      preferences: userData.Preferences,
+      topics: userData.Topics,
+      anything: userData.Anything
+    };
+
+    userMap.set(user.email, user);
+  }
+  return userMap;
+}
+
+function parsePairingFile(filePath: string): Pair[] {
+  const pairFile = fs.readFileSync(new URL(filePath, import.meta.url), 'utf8');
+
+  const config = {
+    quotes: false,
+    quoteChar: '"',
+    escapeChar: '"',
+    delimiter: ",",
+    header: false, //change header to false 
+    newline: "\n",
+    skipEmptyLines: true,
+    columns: null
+  };
+
+  interface Pair{
+    fullNameA: string;
+    emailA: string;
+    fullNameB: string;
+    emailB: string;
+  }
+
+  const results = Papa.parse(pairFile, config);
+
+  const pairings = [];
+
+  for (const pairData of results.data) {
+    const pair: Pair = {
+      fullNameA: pairData[0],
+      emailA: pairData[1],
+      fullNameB: pairData[2],
+      emailB: pairData[3]
+    };
+
+    pairings.push(pair);
+  }
+  return pairings;
+}
+
+//testing
+//console.log(parseUserFile(USER_FILE));
+//console.log(parsePairingFile(PAIR_FILE));
+
+parseUserFile(USER_FILE);
+parsePairingFile(PAIR_FILE);
+
