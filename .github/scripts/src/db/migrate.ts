74f28dccf15802440042f13a53e6d3d5ff5c1053
Andrew <Quark>
2026-06-26 14:40:29 -0400

    Fix the migration pipeline
    - Replaced erroneous schema with values in composite action
    - Make migrate.ts parse the --environment flag to conditionally render
    the right role prefix for the database
    - Fix the path for the migrate.ts script invocation in the composite
    action

--- .github/scripts/src/db/migrate.ts
+++ .github/scripts/src/db/migrate.ts
@@ -1,20 +1,30 @@
+import { EnvClient, EnvClientStrategy } from "@tahminator/pipeline";
 import { $ } from "bun";
-import {
-  EnvClient,
-  EnvClientStrategy,
-} from "@tahminator/pipeline";
-
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 
 async function main() {
+  const { environment } = await yargs(hideBin(process.argv))
+    .option("environment", {
+      type: "string",
+      choices: ["staging", "production"],
+      default: "production",
+    })
+    .option("sha", {
+      type: "string",
+      description: "Current commit SHA (staging only)",
+    })
+    .parseAsync();
+
   const envClient = EnvClient.create(EnvClientStrategy.SOPS);
-  const ciEnv = await envClient.readFromEnv(".env.ci");
+  const ciEnv = await envClient.readFromEnv("secrets-rw.yaml");
   const {
     DATABASE_NAME,
     DATABASE_HOST,
     DATABASE_PORT,
     DATABASE_USER,
     DATABASE_PASSWORD,
-  } = parseCiEnv(ciEnv);
+  } = parseCiEnv(ciEnv, environment);
 
   await $.env({
     ...process.env,
@@ -26,11 +36,13 @@ async function main() {
   })`./mvnw flyway:migrate -Dflyway.locations=filesystem:db`;
 }
 
-function parseCiEnv(ciEnv: Record<string, string>) {
+function parseCiEnv(ciEnv: Record<string, string>, environment: string) {
+  const roleSuffix = environment === "staging" ? "stg" : "prod";
+
   const DATABASE_NAME = (() => {
     const v = ciEnv["PG_DATABASE"];
     if (!v) {
-      throw new Error("Missing PG_DATABASE from .env.ci");
+      throw new Error("Missing PG_DATABASE from secrets-rw.yaml");
     }
     return v;
   })();
@@ -38,7 +50,7 @@ function parseCiEnv(ciEnv: Record<string, string>) {
   const DATABASE_HOST = (() => {
     const v = ciEnv["PG_HOST"];
     if (!v) {
-      throw new Error("Missing PG_HOST from .env.ci");
+      throw new Error("Missing PG_HOST from secrets-rw.yaml");
     }
     return v;
   })();
@@ -46,19 +58,19 @@ function parseCiEnv(ciEnv: Record<string, string>) {
   const DATABASE_PORT = (() => {
     const v = ciEnv["PG_PORT"];
     if (!v) {
-      throw new Error("Missing PG_PORT from .env.ci");
+      throw new Error("Missing PG_PORT from secrets-rw.yaml");
     }
     return v;
   })();
 
-  const DATABASE_USER = (() => {
-    return "patchats-stg-app"
-  })();
+  const DATABASE_USER = `patchats-${roleSuffix}-app`;
 
   const DATABASE_PASSWORD = (() => {
-    const v = ciEnv["PG_ROLE_patchats-stg-app"];
+    const v = ciEnv[`PG_ROLE_patchats-${roleSuffix}-app`];
     if (!v) {
-      throw new Error("Missing PG_PASSWORD from .env.ci");
+      throw new Error(
+        `Missing PG_ROLE_patchats-${roleSuffix}-app from secrets-rw.yaml`,
+      );
     }
     return v;
   })();
