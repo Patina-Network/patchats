7d8238943efb3a0f183e2f824713e2f8483b5754
Allison Lee <esera.lee093@gmail.com>
2026-06-26 16:10:01 -0400

    Added content to UserProfile.page

---
+++ js/src/features/user-profile/UserProfile.page.tsx
@@ -0,0 +1,9 @@
+import { Stack, Title } from "@mantine/core";
+
+export function UserProfilePage() {
+  return (
+    <Stack gap="xl">
+      <Title order={2}>User Profile</Title>
+    </Stack>
+  );
+}
