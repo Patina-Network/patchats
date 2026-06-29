e2241533499aa6821f518438f65c358c7e4dcd12
Allison Lee <esera.lee093@gmail.com>
2026-06-29 13:55:51 -0400

    Member.java done

--- src/main/java/org/patinanetwork/patchats/common/db/models/member/Member.java
+++ src/main/java/org/patinanetwork/patchats/common/db/models/member/Member.java
@@ -1,96 +1,54 @@
-package org.patinanetwork.codebloom.common.db.models.member;
+package org.patinanetwork.patchats.common.db.models.member;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.UUID;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import lombok.experimental.SuperBuilder;
-import org.patinanetwork.codebloom.common.db.helper.annotations.JoinColumn;
-import org.patinanetwork.codebloom.common.db.helper.annotations.NotNullColumn;
-import org.patinanetwork.codebloom.common.db.helper.annotations.NullColumn;
-import org.patinanetwork.codebloom.common.db.models.achievements.Achievement;
-import org.patinanetwork.codebloom.common.db.models.usertag.UserTag;
 
 @Getter
-@Setter
-@SuperBuilder
+@Builder
 @ToString
 @EqualsAndHashCode
 public class Member {
 
-    @NotNullColumn
-    private java.util.UUID id;
+    private UUID id;
 
-    @NotNullColumn
+    @Setter
     private String fullName;
 
-    @NotNullColumn
+    @Setter
     private String email;
 
-    @NullColumn
+    @Setter
     private String linkedInUrl;
 
-    @NotNullColumn
+    @Setter
     private String introduction;
 
-    @NullColumn
+    @Setter
     private String referralSource;
 
-    @NotNullColumn
+    @Setter
     private boolean active;
 
-    @NullColumn
+    @Setter
     private String matchPref;
 
-    @NullColumn
+    @Setter
     private String industryPref;
 
-    @NullColumn
+    @Setter
     private String rolePref;
 
-    @NullColumn
+    @Setter
     private String topics;
 
-    @NullColumn
+    @Setter
     private String extraNotes;
 
-    @NotNullColumn
     private String createdAt;
 
-    @NotNullColumn
     private String updatedAt;
-    /**
-     * If you want to update tags in the database, you have to use the
-     * {@link org.patinanetwork.codebloom.common.db.repos.usertag.UserTagRepository}
-     */
-    @JoinColumn
-    @Builder.Default
-    private List<UserTag> tags = new ArrayList<>();
-
-    /**
-     * If you want to update achievements in the database, you have to use the
-     * {@link org.patinanetwork.codebloom.common.db.repos.achievements.AchievementRepository}
-     */
-    @JoinColumn
-    @Builder.Default
-    private List<Achievement> achievements = new ArrayList<>();
-
-    /**
-     * This operation is permitted, but the tag will not be used in update operations in the UserRepository. Instead
-     * call this method with the parameter being the add method from
-     * {@link org.patinanetwork.codebloom.common.db.repos.usertag.UserTagRepository}
-     *
-     * <p>Essentially, this operation should be used to keep the User model up-to-date with any Tag operations without
-     * needlessly querying the database for the full User object.
-     */
-    public void addTag(final UserTag tag) {
-        if (tag == null) {
-            return;
-        }
-
-        tags.add(tag);
-    }
 }
