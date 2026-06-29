a9d9de88b7e1396a635f15921490e19ca7e48a11
Allison Lee <esera.lee093@gmail.com>
2026-06-29 14:29:52 -0400

    Updated field named in members db, MemberRepo.java done

--- src/main/java/org/patinanetwork/patchats/common/db/repos/member/MemberRepo.java
+++ src/main/java/org/patinanetwork/patchats/common/db/repos/member/MemberRepo.java
@@ -1,1 +1,46 @@
+package org.patinanetwork.patchats.common.db.repos.member;
 
+import java.util.Optional;
+import java.util.UUID;
+import org.patinanetwork.patchats.common.db.models.member.Member;
+
+public interface MemberRepo {
+    /**
+     * @note - The provided object's methods will be overridden with any returned data from the database.
+     * @param member - required fields:
+     *     <ul>
+     *       <li>id
+     *       <li>fullName
+     *       <li>email
+     *       <li>introduction
+     *       <li>createdAt
+     *       <li>updatedAt
+     *     </ul>
+     */
+    Member createMember(Member member);
+
+    /**
+     * @note - The provided object's methods will be overridden with any returned data from the database.
+     * @param member - overridden fields:
+     *     <ul>
+     *       <li>fullName
+     *       <li>email
+     *       <li>linkedInUrl
+     *       <li>introduction
+     *       <li>referralSource
+     *       <li>active
+     *       <li>matchPref
+     *       <li>industryPref
+     *       <li>rolePref
+     *       <li>topics
+     *       <li>extraNotes
+     *     </ul>
+     */
+    boolean editMember(Member member);
+
+    Optional<Member> getMemberById(UUID id);
+
+    Optional<Member> getMemberByEmail(String email);
+
+    boolean deleteMemberById(UUID id);
+}
