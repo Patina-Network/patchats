164cbcda003f6ba5694d8fac4284d06c79d5be41
Allison Lee <esera.lee093@gmail.com>
2026-07-06 14:40:14 -0400

    added dto records CreateMemberRequest and UpdateMemberRequest. Updated MemberService to convert requests into members for createMember and updateMember.

--- src/main/java/org/patinanetwork/patchats/api/member/MemberService.java
+++ src/main/java/org/patinanetwork/patchats/api/member/MemberService.java
@@ -4,7 +4,9 @@ import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.patinanetwork.patchats.common.db.models.member.Member;
 import org.patinanetwork.patchats.common.db.repos.member.MemberRepo;
+import org.patinanetwork.patchats.common.dto.member.CreateMemberRequest;
 import org.patinanetwork.patchats.common.dto.member.MemberDto;
+import org.patinanetwork.patchats.common.dto.member.UpdateMemberRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -15,22 +17,47 @@ public class MemberService {
 
     private final MemberRepo memberRepo;
 
-    public MemberDto createMember(Member member) {
-        if (memberRepo.getMemberByEmail(member.getEmail()).isPresent()) {
+    public MemberDto createMember(CreateMemberRequest request) {
+        if (memberRepo.getMemberByEmail(request.email()).isPresent()) {
             throw new ResponseStatusException(
-                    HttpStatus.CONFLICT, "Member with email " + member.getEmail() + " already exists");
+                    HttpStatus.CONFLICT, "Member with email " + request.email() + " already exists");
         }
-        member.setId(UUID.randomUUID());
-        member.setActive(true);
+        Member member = Member.builder()
+                .id(UUID.randomUUID())
+                .fullName(request.fullName())
+                .email(request.email())
+                .linkedInUrl(request.linkedInUrl())
+                .introduction(request.introduction())
+                .referralSource(request.referralSource())
+                .active(true)
+                .matchPref(request.matchPref())
+                .industryPref(request.industryPref())
+                .rolePref(request.rolePref())
+                .topics(request.topics())
+                .extraNotes(request.extraNotes())
+                .build();
         Member createdMember = memberRepo.createMember(member);
         return MemberDto.from(createdMember);
     }
 
-    public MemberDto editMember(Member member) {
-        return memberRepo
-                .editMember(member)
-                .map(MemberDto::from)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Member not found"));
+    public MemberDto updateMember(UpdateMemberRequest request, UUID id) {
+        if (memberRepo.getMemberById(id).isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Member with ID " + id + " does not exist");
+        }
+        Member member = Member.builder()
+                .id(id)
+                .fullName(request.fullName())
+                .email(request.email())
+                .linkedInUrl(request.linkedInUrl())
+                .introduction(request.introduction())
+                .matchPref(request.matchPref())
+                .industryPref(request.industryPref())
+                .rolePref(request.rolePref())
+                .topics(request.topics())
+                .extraNotes(request.extraNotes())
+                .build();
+        Member updatedMember = memberRepo.updateMember(member);
+        return MemberDto.from(updatedMember);
     }
 
     public MemberDto getMemberById(UUID id) {
