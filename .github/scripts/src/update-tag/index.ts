8d3c4a7f9e66a871adf18c8c6d3d0ea07bb8b17b
Arshadul Monir <arshadulmonir@gmail.com>
2026-07-10 15:13:19 -0400

    ci: Added auto k8s PR for staging

---
+++ .github/scripts/src/update-tag/index.ts
@@ -0,0 +1,69 @@
+import { GitHubClient, Utils, type Environment } from "@tahminator/pipeline";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+const { environment, newTagVersion } = await yargs(hideBin(process.argv))
+  .option("newTagVersion", {
+    type: "string",
+    demandOption: true,
+  })
+  .option("environment", {
+    choices: ["staging", "production"] satisfies Environment[],
+    describe: "Deployment environment (staging or production)",
+    demandOption: true,
+  })
+  .strict()
+  .parse();
+
+async function main() {
+  const githubAppAppId = process.env._GITHUB_APP_APP_ID;
+  const githubAppInstallationId = process.env._GITHUB_APP_INSTALLATION_ID;
+  const githubAppPrivateKeyB64 = process.env._GITHUB_APP_PEM_CONTENT;
+
+  if (!githubAppAppId) {
+    throw new Error("Missing _GITHUB_APP_APP_ID from process env");
+  }
+  if (!githubAppInstallationId) {
+    throw new Error("Missing _GITHUB_APP_INSTALLATION_ID from process env");
+  }
+  if (!githubAppPrivateKeyB64) {
+    throw new Error("Missing _GITHUB_APP_PEM_CONTENT from process env");
+  }
+
+  const ghClient = await GitHubClient.createWithGithubAppToken({
+    appId: githubAppAppId,
+    installationId: githubAppInstallationId,
+    privateKey: await Utils.decodeBase64EncodedString(githubAppPrivateKeyB64),
+  });
+
+  if (environment === "production") {
+    await ghClient.updateK8sTagWithPR({
+      manifestRepo: ["Patina-Network", "k8s-manifest"],
+      originRepo: ["Patina-Network", "patchats"],
+      kustomizationFilePath: "base/production/patchats/kustomization.yaml",
+      imageName: "Patina-Network/patchats",
+      newTag: newTagVersion,
+      environment: "production",
+    });
+  }
+
+  if (environment === "staging") {
+    await ghClient.updateK8sTagWithPR({
+      manifestRepo: ["Patina-Network", "k8s-manifest"],
+      originRepo: ["Patina-Network", "patchats"],
+      kustomizationFilePath: "base/staging/patchats/kustomization.yaml",
+      imageName: "Patina-Network/patchats",
+      newTag: newTagVersion,
+      environment: "staging",
+    });
+  }
+}
+
+main()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
