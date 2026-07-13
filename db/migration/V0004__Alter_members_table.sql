3b1db11cc962aae183af50e7064c4a562e91fcd7
Allison Lee <esera.lee093@gmail.com>
2026-07-13 13:02:48 -0400

    Added V0004 to alter members table

---
+++ db/migration/V0004__Alter_members_table.sql
@@ -0,0 +1,7 @@
+ALTER TABLE members RENAME COLUMN linked_url TO linked_in_url;
+ALTER TABLE members RENAME COLUMN bio TO introduction;
+ALTER TABLE members RENAME COLUMN industry TO industry_pref;
+ALTER TABLE members RENAME COLUMN role TO role_pref;
+
+ALTER TABLE members ALTER COLUMN introduction SET NOT NULL;
+ALTER TABLE members ALTER COLUMN active SET NOT NULL DEFAULT TRUE;
