15b2a7a234fc385137a7a99b8cf1ec4a9e09fc48
Arshadul Monir <arshadulmonir@gmail.com>
2026-07-26 15:01:07 -0400

    CI: simplified logic in build-image script
    checkstyle for build-image

--- .github/scripts/src/build-image/index.ts
+++ .github/scripts/src/build-image/index.ts
@@ -1,26 +1,39 @@
-import type { Environment } from "@tahminator/pipeline";
-
+import {
+  DockerClient,
+  GitHubClient,
+  type Environment,
+} from "@tahminator/pipeline";
 import { $ } from "bun";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
 process.env.TZ = "America/New_York";
 
-const { environment, githubOutputFile } = await yargs(hideBin(process.argv))
-  .option("environment", {
-    type: "string",
-    choices: ["staging", "production"] satisfies Environment[],
-    demandOption: true,
-  })
-  .option("githubOutputFile", {
-    type: "string",
-    describe: "Path to GITHUB_OUTPUT (passed in automatically in CI)",
-    default: process.env.GITHUB_OUTPUT,
-  })
-  .parseAsync();
-
-const tagPrefix = environment === "staging" ? "staging-" : "";
-const serverProfiles = environment === "staging" ? "stg" : "prod";
+const { environment, getGhaOutput, githubOutputFile, dockerFileName } =
+  await yargs(hideBin(process.argv))
+    .option("environment", {
+      choices: ["staging", "production"] satisfies Environment[],
+      describe: "Deployment environment (staging or production)",
+      demandOption: true,
+    })
+    .option("getGhaOutput", {
+      type: "boolean",
+      describe:
+        "Enable GitHub Actions output to receive latest built tag version",
+      default: false,
+    })
+    .option("githubOutputFile", {
+      type: "string",
+      describe:
+        "Path to GITHUB_OUTPUT (this will be passed in automatically in CI)",
+      default: process.env.GITHUB_OUTPUT,
+    })
+    .option("dockerFileName", {
+      type: "string",
+      default: "Dockerfile.web",
+    })
+    .strict()
+    .parse();
 
 async function main() {
   const DOCKER_HUB_PAT = process.env.DOCKER_HUB_PAT;
@@ -28,6 +41,14 @@ async function main() {
   if (!DOCKER_HUB_PAT) {
     throw new Error("Missing DOCKER_HUB_PAT from platform-infra patchats.yaml");
   }
+
+  const ghClient = await GitHubClient.createWithDefaultCiToken();
+  await using dockerClient = await DockerClient.create(
+    "patinanetwork",
+    DOCKER_HUB_PAT,
+  );
+
+  const tagPrefix = environment === "staging" ? "staging-" : "";
 
   const timestamp = new Date()
     .toLocaleString("en-US", {
@@ -44,59 +65,19 @@ async function main() {
 
   const gitSha = (await $`git rev-parse --short HEAD`.text()).trim();
 
-  const tags = [
-    `patinanetwork/patchats:${tagPrefix}latest`,
-    `patinanetwork/patchats:${tagPrefix}${timestamp}`,
-    `patinanetwork/patchats:${tagPrefix}${gitSha}`,
-  ];
+  await dockerClient.buildImage({
+    dockerRepository: `patchats`,
+    dockerFileLocation: `infra/${dockerFileName}`,
+    tags: [`${tagPrefix}${timestamp}`, `${tagPrefix}${gitSha}`],
+  });
 
-  console.log("Building image with following tags:");
-  tags.forEach((tag) => console.log(tag));
-
-  const proc = Bun.spawn(
-    ["docker", "login", "-u", "patinanetwork", "--password-stdin"],
-    {
-      stdin: new TextEncoder().encode(DOCKER_HUB_PAT),
-      stdout: "inherit",
-      stderr: "inherit",
-    },
-  );
-
-  const exitCode = await proc.exited;
-
-  if (exitCode !== 0) {
-    throw new Error("Docker login failed");
-  }
-  try {
-    await $`docker buildx create --use --name patchats-builder`;
-  } catch {
-    await $`docker buildx use patchats-builder`;
-  }
-
-  const viteStagingArg =
-    serverProfiles === "stg" ? ["--build-arg", "VITE_STAGING=true"] : [];
-
-  const tagArgs = tags.flatMap((tag) => ["--tag", tag]);
-
-  await $`docker buildx build --push \
-            --platform linux/amd64 \
-            --file infra/Dockerfile \
-            --build-arg SERVER_PROFILES=${serverProfiles} \
-            --build-arg COMMIT_SHA=${gitSha} \
-            --cache-from=type=gha \
-            --cache-to=type=gha,mode=max \
-            ${viteStagingArg} \
-            ${tagArgs} \
-            .`;
-
-  console.log("Image pushed successfully.");
-
-  if (githubOutputFile) {
-    console.log("Outputting image tag...");
-    const w = Bun.file(githubOutputFile).writer();
-    await w.write(`tag<<EOF\n${tagPrefix}${gitSha}\nEOF\n`);
-    await w.flush();
-    await w.end();
+  if (getGhaOutput) {
+    await ghClient.outputToGithubOutput({
+      overrideGithubOutputFile: githubOutputFile ? githubOutputFile : undefined,
+      ctx: {
+        tag: `${tagPrefix}${gitSha}`,
+      },
+    });
   }
 }
 
