e4b6dc167230d281db283be90feaf539d549d60a
andrew <52230726+rootandroo@users.noreply.github.com>
2026-08-07 14:08:27 -0400

    Implement Match & MatchCycle Repo (#53)

---
+++ src/main/java/org/patinanetwork/patchats/api/match/db/repos/MatchCycleFilterCriteria.java
@@ -0,0 +1,12 @@
+package org.patinanetwork.patchats.api.match.db.repos;
+
+import java.time.Instant;
+import java.util.Optional;
+
+public record MatchCycleFilterCriteria(
+        Optional<String> period, Optional<Instant> startTime, Optional<Instant> endTime) {
+
+    public static MatchCycleFilterCriteria empty() {
+        return new MatchCycleFilterCriteria(Optional.empty(), Optional.empty(), Optional.empty());
+    }
+}
---
+++ src/main/java/org/patinanetwork/patchats/api/match/db/repos/MatchRepo.java
@@ -0,0 +1,45 @@
+package org.patinanetwork.patchats.api.match.db.repos;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import org.patinanetwork.patchats.api.match.db.models.Match;
+
+public interface MatchRepo {
+    /**
+     * @note - The provided object's methods will be overridden with any returned data from the database.
+     * @param match - required fields:
+     *     <ul>
+     *       <li>id
+     *       <li>memberAId
+     *       <li>memberBId
+     *       <li>matchCycleId
+     *     </ul>
+     */
+    Match createMatch(Match match);
+
+    /**
+     * @note - The provided object's methods will be overridden with any returned data from the database.
+     * @param match - overridden fields:
+     *     <ul>
+     *       <li>memberAId
+     *       <li>memberBId
+     *       <li>matchCycleId
+     *       <li>matchScore
+     *       <li>status
+     *       <li>feedbackA
+     *       <li>feedbackB
+     *     </ul>
+     */
+    Optional<Match> updateMatch(Match match);
+
+    Optional<Match> getMatchById(UUID id);
+
+    Optional<Match> setMatchStatus(UUID id, String status);
+
+    Optional<Match> deleteMatchById(UUID id);
+
+    Optional<Match> recordFeedback(UUID id, UUID memberId, String feedback);
+
+    List<Match> filterMatches(MatchFilterCriteria criteria);
+}
