cddba849783c51a0ea0772fa302db11997661bec
Arshadul Monir <arshadulmonir@gmail.com>
2026-09-12 08:40:40 -0400

    Enable db ci (#104)

--- .github/scripts/src/test/run-backend-tests.ts
+++ .github/scripts/src/test/run-backend-tests.ts
@@ -1,7 +1,29 @@
+import { LocalPostgresClient } from "@tahminator/pipeline";
 import { $ } from "bun";
 
+import { migrateDb } from "../db/index.ts";
+
 async function main() {
-  await $`./mvnw clean test -Dspring.profiles.active=ci`;
+  await using pgClient = await LocalPostgresClient.create({
+    database: "patchats-ci",
+  });
+
+  const { database, host, port, password, user } = pgClient.state;
+
+  const databaseEnvironment = {
+    DATABASE_HOST: host,
+    DATABASE_NAME: database,
+    DATABASE_PASSWORD: password,
+    DATABASE_PORT: String(port),
+    DATABASE_USER: user,
+  };
+
+  await migrateDb(databaseEnvironment);
+
+  await $.env({
+    ...process.env,
+    ...databaseEnvironment,
+  })`./mvnw clean test -Dspring.profiles.active=ci`;
 }
 
 main()
