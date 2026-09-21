fa7b113e0c9e70a53d72f192aa7446b1bfcbefdc
andrew <52230726+rootandroo@users.noreply.github.com>
2026-09-21 11:33:38 -0400

    Optimize pages for mobile (#111)
    Builds on top of and closes Kelly's PR  https://app.graphite.com/github/pr/Patina-Network/patchats/91/Added-Home-Page-Content-and-Mock-Grid. 
    
    Merged here for convience.
    
    ## Endpoint Screenshots
    
    / BEFORE desktop
    
    ![image.png](https://app.graphite.com/user-attachments/assets/19b0a6fb-e693-496b-b3e4-6ab9bddc27e4.png)
    
    / AFTER desktop
    
    ![image.png](https://app.graphite.com/user-attachments/assets/fb02e7d6-83ee-465a-8ea5-4f60973bfe78.png)
    
    / BEFORE mobile
    
    ![image.png](https://app.graphite.com/user-attachments/assets/5f5cd2df-18f5-4f04-ba42-1b96047cae52.png)
    
    / AFTER mobile
    
    ![image.png](https://app.graphite.com/user-attachments/assets/5f88954b-511a-45ff-b212-978738d0cda8.png)
    
    /sign-up BEFORE desktop
    
    ![image.png](https://app.graphite.com/user-attachments/assets/7fe044b5-fad9-4f07-a3df-6a4edd9f4825.png)
    
    /sign-up AFTER desktop
    
    ![image.png](https://app.graphite.com/user-attachments/assets/24788c2e-2b38-4d26-9b40-a45fa7a5fbb3.png)
    
    /sign-up BEFORE mobile
    
    ![image.png](https://app.graphite.com/user-attachments/assets/c7665622-4d7f-4c18-9d13-8edff62cdd9f.png)
    
    /sign-up AFTER mobile
    
    ![image.png](https://app.graphite.com/user-attachments/assets/ef047a69-6438-4124-910c-c03eec45d9fb.png)

--- js/src/features/member-profile/MemberProfile.page.tsx
+++ js/src/features/member-profile/MemberProfile.page.tsx
@@ -1,5 +1,5 @@
 import { MemberProfileForm } from "@/features/member-profile/components/MemberProfileForm";
-import { Stack, Title, Alert } from "@mantine/core";
+import { Alert, Container, Stack, Title } from "@mantine/core";
 import { useParams } from "react-router-dom";
 
 export function MemberProfilePage() {
@@ -7,9 +7,11 @@ export function MemberProfilePage() {
   if (!id) return <Alert color="red">No member ID provided.</Alert>;
 
   return (
-    <Stack gap="xl">
-      <Title order={2}>Member Profile</Title>
-      <MemberProfileForm id={id} />
-    </Stack>
+    <Container py="xl" size="sm">
+      <Stack gap="xl">
+        <Title order={2}>Member Profile</Title>
+        <MemberProfileForm id={id} />
+      </Stack>
+    </Container>
   );
 }
