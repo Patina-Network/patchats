2df938a7b2e1d5763e08edd88aac89d584e0a235
Brian Calle <143109840+brian10101@users.noreply.github.com>
2026-09-25 20:01:34 -0400

    Addition: Brian in authors array list

--- src/main/java/org/patinanetwork/patchats/utilities/ServerMetadataObject.java
+++ src/main/java/org/patinanetwork/patchats/utilities/ServerMetadataObject.java
@@ -15,7 +15,7 @@ public class ServerMetadataObject {
         this.name = "PatChats";
         this.version = Strings.isNullOrEmpty(commitSha) ? "unknown" : commitSha;
         this.description = "";
-        this.authors = new ArrayList<>(Arrays.asList("Sam Lee", "Fahim Rashid"));
+        this.authors = new ArrayList<>(Arrays.asList("Sam Lee", "Fahim Rashid", "Brian Calle"));
     }
 
     public ServerMetadataObject() {
